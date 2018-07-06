82ba0a9068bf3c757befb4ebfcb83829fec3f164
SakinaSadigova <36983955+SakinaSadigova@users.noreply.github.com>
2018-07-06 07:32:12 -0700

    Master page and views created.The links added

---
+++ BanzHow/Controllers/OtherPagesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BanzHow.Controllers
+{
+    public class OtherPagesController : Controller
+    {
+        // GET: OtherPages
+        public ActionResult FullWidth()
+        {
+            return View();
+        }
+        public ActionResult Sidebar()
+        {
+            return View();
+        }
+        public ActionResult Faq()
+        {
+            return View();
+        }
+        public ActionResult Error404()
+        {
+            return View();
+        }
+    }
+}
---
+++ BanzHow/Controllers/PortfolioController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BanzHow.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        // GET: Portfolio
+        public ActionResult PortfolioOneCol()
+        {
+            return View();
+        }
+        public ActionResult PortfolioTwoCol()
+        {
+            return View();
+        }
+        public ActionResult PortfolioThreeCol()
+        {
+            return View();
+        }
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
